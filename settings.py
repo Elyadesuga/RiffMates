1027fbaf50479f77a51bc4a1b9ed15a35b094896
Elyadesuga <elyadesuga@gmail.com>
2025-04-27 13:31:12 +0700

    first page

--- settings.py
+++ settings.py
@@ -1,13 +1,5 @@
 import os
-
-# Базовые настройки для разработки и production
-ALLOWED_HOSTS = [
-    'riffmates-v4ph.onrender.com',  # Ваш конкретный хост
-    '.onrender.com',                # Все поддомены Render
-    'localhost',
-    '127.0.0.1',
-    '[::1]'                         # IPv6 localhost
-]
+ALLOWED_HOSTS = ['*']
 
 # Дополнительная защита для production
 if os.environ.get('DEBUG', 'False') == 'False':
