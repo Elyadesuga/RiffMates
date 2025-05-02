8dc8088a9dda652d449cfb5b695954e4df5c33d3
Elyadesuga <elyadesuga@gmail.com>
2025-05-02 21:25:44 +0700

    credits page

--- home/views.py
+++ home/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def credits(request):
-    content "Nicky\nElmira"
+    content = "Nicky\nElmira"
     return HttpResponse(content, content_type="text/plain")
