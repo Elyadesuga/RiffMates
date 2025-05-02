a99e5a5a0b26b3f41ee5fd60aa9cc8c1d2795ed4
Elyadesuga <elyadesuga@gmail.com>
2025-05-02 21:22:57 +0700

    credits page

--- RiffMates/urls.py
+++ RiffMates/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from home import view as home_views
+from home import views as home_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
